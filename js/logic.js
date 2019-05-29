50ae3fda503759b77a0c099aecf5c46bc9196e8e
jordan Vera <jordanvera@jordans-iMac.local>
2019-05-29 11:43:48 -0500

    added all service area pages by county and city as well as stylesheet updates

---
+++ js/logic.js
@@ -0,0 +1,71 @@
+// Google API key =  AIzaSyC_-wcnVQ5w2Kh8Rh5BzC3vNbKnJoNroYo 
+
+function initMap() {
+	var options = {
+		zoom: 8,
+		center: {lat: 29.76328, lng:-95.36327}
+	}
+	
+	var map = new google.maps.Map(document.getElementById('map'), options)
+	
+	const houston = [
+		{lat: 29.035821, lng: -95.275498},
+		{lat: 29.780502, lng: -95.953527},
+		{lat: 30.364618, lng: -95.542913},
+		{lat: 30.407936, lng: -95.264307},
+		{lat: 29.819916, lng: -94.899012},
+		{lat: 29.360535, lng: -94.850775},
+		{lat: 29.035821, lng: -95.275498}
+	];
+	
+	
+	
+	const beaumont = [
+		{lat: 30.099893, lng: -94.251885},
+		{lat: 30.187774, lng: -94.146828},
+		{lat: 30.024717, lng: -93.971390},
+		{lat: 29.965844, lng: -93.823075},
+		{lat: 29.789015, lng: -93.960061},
+		{lat: 30.099893, lng: -94.251885}
+	];
+	
+	const collegeStation = [
+	
+		{lat: 30.593193, lng: -96.430804},
+		{lat: 30.716486, lng: -96.373126},
+		{lat: 30.651493, lng: -96.227204},
+		{lat: 30.544483, lng: -96.258756},
+		{lat: 30.544483, lng: -96.427465},
+		{lat: 30.593193, lng: -96.430804}
+	];
+	
+	let polygon = new google.maps.Polygon({
+	          paths: houston,
+	          strokeColor: '#FF0000',
+	          strokeOpacity: 0.4,
+	          strokeWeight: 2,
+	          fillColor: '#FF0000',
+	          fillOpacity: 0.1
+	        });
+	        polygon.setMap(map);
+	
+	polygon = new google.maps.Polygon({
+	          paths: beaumont,
+	          strokeColor: '#FF0000',
+	          strokeOpacity: 0.4,
+	          strokeWeight: 2,
+	          fillColor: '#FF0000',
+	          fillOpacity: 0.1
+	        });
+	        polygon.setMap(map);
+			
+	polygon = new google.maps.Polygon({
+	          paths: collegeStation,
+	          strokeColor: '#FF0000',
+	          strokeOpacity: 0.4,
+	          strokeWeight: 2,
+	          fillColor: '#FF0000',
+	          fillOpacity: 0.1
+	        });
+	        polygon.setMap(map);
+}
